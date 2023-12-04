6c363daa2af87f9e9ed20307d71d32fa17fdf332
profitsw2000 <profitsw2000@gmail.com>
2023-12-04 12:46:10 +0300

    Во фрагмент MainFragment добавлены основные функции для работы с данными.

--- mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/MainFragment.kt
+++ mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/MainFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import diarynote.mainfragment.R
 import diarynote.mainfragment.databinding.FragmentMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.profitsw2000.core.view.CoreFragment
+import ru.profitsw2000.core.viewmodel.CoreViewModel
+import ru.profitsw2000.data.model.MathCategoryModel
+import ru.profitsw2000.mainfragment.presentation.viewmodel.MainViewModel
 
 class MainFragment : CoreFragment(R.layout.fragment_main) {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    override val viewModel: MainViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +28,23 @@ class MainFragment : CoreFragment(R.layout.fragment_main) {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        observeData()
+        viewModel.getCategoryList()
+    }
 
+    private fun initViews() = with(binding) {
+
+    }
+
+    private fun observeData() {
+        val observer = Observer<List<MathCategoryModel>> { renderData(it) }
+        viewModel.categoryLiveData.observe(viewLifecycleOwner, observer)
+    }
+
+    private fun renderData(mathCategoryModelList: List<MathCategoryModel>) {
+
+    }
 }
