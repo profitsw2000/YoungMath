5c6c11bf9676acc54b229a1f000cca33757badbf
profitsw2000 <profitsw2000@gmail.com>
2023-12-04 11:52:22 +0300

    Разработка главного фрагмента - начало.

--- mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/MainFragment.kt
+++ mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/MainFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.profitsw2000.mainfragment.R
+import diarynote.mainfragment.R
+import diarynote.mainfragment.databinding.FragmentMainBinding
+import ru.profitsw2000.core.view.CoreFragment
 
-class MainFragment : Fragment() {
+class MainFragment : CoreFragment(R.layout.fragment_main) {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.bind(inflater.inflate(R.layout.fragment_main, container, false))
+        return binding.root
     }
+
+
 }
