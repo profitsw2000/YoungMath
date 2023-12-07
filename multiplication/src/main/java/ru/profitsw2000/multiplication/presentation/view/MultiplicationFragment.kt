fa22b8226d39a0461923ae6f20c5515372388e2a
profitsw2000 <profitsw2000@gmail.com>
2023-12-07 10:41:01 +0300

    Сделан макет стартового экрана раздела по проверке таблицы умножения.

---
+++ multiplication/src/main/java/ru/profitsw2000/multiplication/presentation/view/MultiplicationFragment.kt
@@ -0,0 +1,19 @@
+package ru.profitsw2000.multiplication.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.profitsw2000.multiplication.R
+
+class MultiplicationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_multiplication, container, false)
+    }
+}
