95f84fdd6f6603c278522fb05053ffd5b79e8a0d
profitsw2000 <profitsw2000@gmail.com>
2023-12-08 18:13:31 +0300

    На основе интерфейса Navigator создан класс NavigatorImpl.

---
+++ app/src/main/java/ru/profitsw2000/youngmath/navigation/NavigatorImpl.kt
@@ -0,0 +1,10 @@
+package ru.profitsw2000.youngmath.navigation
+
+import androidx.navigation.NavController
+import youngmath.navigator.Navigator
+
+class NavigatorImpl(
+    private val navController: NavController
+) : Navigator {
+
+}
