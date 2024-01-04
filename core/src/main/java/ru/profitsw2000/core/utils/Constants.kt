e7a337c2a9c95093939a7e7dd49a58370ffecb5a
profitsw2000 <profitsw2000@gmail.com>
2024-01-04 16:06:59 +0300

    Прописаны ключи для хранения и чтения данных в/из SharedPreferences.

--- core/src/main/java/ru/profitsw2000/core/utils/Constants.kt
+++ core/src/main/java/ru/profitsw2000/core/utils/Constants.kt
@@ -1,3 +1,9 @@
 package ru.profitsw2000.core.utils
 
 val SHARED_PREFERENCE_NAME = "SHARED_PREFERENCE_NAME"
+val MULTIPLICATION_TASK_TIME_KEY = "mult_task_time"
+val MULTIPLICATION_TASKS_NUMBER_KEY = "mult_tasks_number"
+val MULTIPLICATION_FIVE_ASSESSMENT_ERRORS_NUMBER_KEY = "mult_five_assessment_errors_num"
+val MULTIPLICATION_FOUR_ASSESSMENT_ERRORS_NUMBER_KEY = "mult_four_assessment_errors_num"
+val MULTIPLICATION_THREE_ASSESSMENT_ERRORS_NUMBER_KEY = "mult_three_assessment_errors_num"
+val MULTIPLICATION_HIGH_DIFFICULTY_FLAG_KEY = "mult_high_difficulty"
